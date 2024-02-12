63a563b617d3d4c1a1a044ca382044966a1a1445
nick <nick.schneider@mmtn-schneider.com>
2024-02-12 21:27:17 +0100

    Added Reservierung

--- src/pages/Reservierung.js
+++ src/pages/Reservierung.js
@@ -2,6 +2,11 @@
 
 import { useState } from "react"
 import Navbar from "../components/Navbar"
+import Footer from '../components/Footer'
+import FancyInput from '../components/FancyInput'
+import DatePicker from 'react-datepicker'
+
+import "react-datepicker/dist/react-datepicker.css"
 
 
 export default function Reservierung() {
@@ -12,20 +17,109 @@ export default function Reservierung() {
 	const [ phone, setPhone ] = useState()
 
 	const [ numberOfPeople, setNumberOfPeople ] = useState()
-	const [ namesOfPeople, setNamesOfPeople ] = useState([])
-	const [ additionalInformation, setAdditionalInformation ] = useState()
+	const [ date, setDate ] = useState()
 
 
 
 
-	
+
     return (
         <>
             <Navbar/>
 
 			<div className='site'>
 
+				<div className='site-content' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
 
+
+					<div style={{ display: 'flex', flexDirection: 'column' }}>
+
+						<h2>Reservierung</h2>
+
+						<FancyInput
+							id='FirstName'
+							type='text'
+							text='Vorname'
+							value={firstName}
+							setValue={setFirstName}
+							isRequired={true}
+							
+						/>
+						
+						<FancyInput
+							id='LastName'
+							type='text'
+							text='Nachname'
+							value={lastName}
+							setValue={setLastName}
+							isRequired={true}
+						/>
+
+						<FancyInput
+							id='EMail'
+							type='text'
+							text='E-Mail'
+							value={email}
+							setValue={setEmail}
+							isRequired={true}
+						/>
+
+						<FancyInput
+							id='Phone'
+							type='number'
+							text='Telefonnummer'
+							value={phone}
+							setValue={setPhone}
+							isRequired={true}
+						/>
+
+						<div 
+							style={{ 
+								display: 'flex', 
+								justifyContent: 'space-between',
+								padding: '10px 1px 15px 1px' 
+							}}
+						>
+							<label 
+								style={{ 
+									fontSize: '15px', 
+									color: 'var(--navbar-color)' 
+									}}
+								>Datum:</label>
+							<DatePicker selected={date} onChange={(date) => setDate(date)}/>
+						</div>
+
+						<div 
+							style={{ 
+								display: 'flex', 
+								justifyContent: 'space-between',
+								padding: '0px 1px 10px 1px' 
+							}}
+						>
+							<label 
+								style={{ 
+									fontSize: '15px', 
+									color: 'var(--navbar-color)' 
+									}}
+								>Anzahl an Personen:</label>
+							<input 
+								type='number' 
+								style={{ 
+									outline: 'none', 
+									width: '50px', 
+									
+									color: 'var(--navbar-color)' 
+								}}
+							/>
+						</div>
+
+						<button className='button'>Reservierung abschicken</button>
+
+					</div>
+
+				</div>
+
+				<Footer/>
 
 			</div>
             
